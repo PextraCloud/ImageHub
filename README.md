3b8f77d0009330a1bcd1213f483399f2074cc3f1
recanman <29310982+recanman@users.noreply.github.com>
2025-01-26 15:27:00 -0800

    feat: initial version

---
+++ README.md
@@ -0,0 +1,40 @@
+# Pextra ImageHub™
+
+### Access the image library [in the Pextra CloudEnvironment® web UI](https://docs.pextra.cloud/TODO) or visit [ImageHub™ directly](https://imagehub.pextra.cloud/).
+### If you would like to request an image, please [open an issue](https://github.com/PextraCloud/ImageHub/issues) or [start a discussion](https://github.com/PextraCloud/ImageHub/discussions).
+---
+
+ImageHub™ provides a curated list of instance images for [Pextra CloudEnvironment®](https://pextra.cloud/products/cloudenvironment), for a one-click deployment experience of your favorite operating systems and applications.
+
+**Note**: Images provided by ImageHub™ may be used on other private or public cloud platforms, but the deployment experience may vary. Only use with Pextra CloudEnvironment® is supported.
+
+## Development
+
+A simple Python script is provided to generate the image library from the source files. A GitHub workflow also automatically runs to generate the image library upon pushes to `master`, creating a new release.
+
+### Dependencies
+- Python 3.6 or higher
+- `requests` library
+
+### Steps
+1. Clone this repository.
+2. Install the `requests` library by running `pip install requests`.
+
+### Running
+1. Run `python main.py` to start the program. It will read source files from the `sources` directory.
+2. The generated image library will be saved to `output/library.json`.
+
+## Support/Contact
+
+For enterprise licensing, support, and consulting, please visit [our website](https://pextra.cloud/enterprise). Alternatively, you can contact us at [enterprise@pextra.cloud](mailto:support@pextra.cloud).
+
+If you have any questions, please feel free to open an issue or a discussion. You can also contact us at [support@pextra.cloud](mailto:support@pextra.cloud).
+
+## Contributions
+
+We welcome contributions! If you find any bugs, have feature requests, or would like to contribute enhancements, please feel free to open issues or submit pull requests.
+
+## License
+
+ImageHub™ is licensed under the [Apache License 2.0](LICENSE).
+
---
+++ main.py
@@ -0,0 +1,133 @@
+# Copyright 2025 Pextra Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import json, requests, os
+from urllib.parse import urlparse
+
+from typing import TypedDict
+
+def open_file(file_path: str):
+	directory = os.path.dirname(file_path)
+
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+	return open(file_path, 'w')
+
+def read_sources():
+	with open('sources/iso_list.json') as json_file:
+		return json.load(json_file)
+
+def get_final_url_and_content_length(url: str, depth=0):
+	if depth > 5:
+		raise requests.RequestException("Too many redirects")
+	else:
+		response = requests.head(url)
+		if response.status_code == 302:
+			next_url = response.headers['Location']
+			if next_url.startswith('/'):
+				# Handle relative redirects
+				parsed_url = urlparse(url)
+				next_url = f"{parsed_url.scheme}://{parsed_url.netloc}{next_url}"
+
+			return get_final_url_and_content_length(next_url, depth + 1)
+		elif response.status_code != 200:
+			raise requests.RequestException(f"Unexpected status code {response.status_code}")
+
+		print(f" {response.status_code} ({depth} redirects)")
+		return url, response.headers.get('Content-Length')
+
+class ProbeIsoImagePayload(TypedDict):
+	version: str
+	long_version: str # optional
+	architecture: str
+	url_format: str
+class ProbedIsoImage(TypedDict):
+	download_url: str
+	content_length: str
+def probe_image(iso: ProbeIsoImagePayload) -> ProbedIsoImage:
+	# Default long_version to version if not present
+	if "long_version" not in iso:
+		iso["long_version"] = iso["version"]
+	formatted_url = iso["url_format"].format(version=iso["version"], arch=iso["architecture"], long_version=iso["long_version"])
+
+	# Follow redirects to get final URL
+	final_url = formatted_url
+	try:
+		response = get_final_url_and_content_length(formatted_url)
+		final_url = response[0]
+		content_length = response[1]
+	except requests.RequestException:
+		raise Exception(f"Failed to probe {formatted_url}!")
+
+	return {
+		"download_url": final_url,
+		"content_length": content_length
+	}
+
+class IsoImageRequirements(TypedDict):
+	cpu: int
+	memory: int
+	disk: int
+
+class IsoImage(TypedDict):
+	name: str
+	description: str
+	versions: list[str]
+	long_versions: list[str] # optional
+	architectures: list[str]
+	url_format: str
+	# url_format_{arch} can be used to override url_format for specific architectures
+	requirements: IsoImageRequirements # optional
+class IsoList(TypedDict):
+	iso_images: list[IsoImage]
+def build_iso_list(iso_list: IsoList):
+	iso_imgs = {}
+	for iso in iso_list:
+		iso_name = iso["name"]
+
+		print(f"\nBuilding list for {iso_name}...")
+		iso_imgs[iso_name] = {
+			"name": iso_name,
+			"description": iso["description"],
+			"requirements": iso.get("requirements", {}),
+			"downloads": {}
+		}
+
+		for i, version in enumerate(iso["versions"]):
+			long_version = iso["long_versions"][i] if "long_versions" in iso else version
+
+			iso_imgs[iso_name]["downloads"][version] = {}
+			for architecture in iso["architectures"]:
+				print(f"\tProbing {version} ({architecture})...", end='')
+				probe_payload: ProbeIsoImagePayload = {
+					"version": version,
+					"long_version": long_version,
+					"architecture": architecture,
+					"url_format": iso.get(f"url_format_{architecture}", iso["url_format"]) # try url_format_{arch}, fallback to url_format
+				}
+
+				probed_iso = probe_image(probe_payload)
+				iso_imgs[iso_name]["downloads"][version][architecture] = probed_iso
+
+	return {
+		"iso_images": iso_imgs
+	}
+
+iso_list = read_sources()
+result = build_iso_list(iso_list)
+
+output_file = open_file('output/library.json')
+output_file.write(json.dumps(result, indent=2))
+output_file.close()
+
+print("ISO images JSON structure created successfully.")
